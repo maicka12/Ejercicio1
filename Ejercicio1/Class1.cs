b363d86b827ed14949bf80e2a1764ec89fe760b8
maicka <maicka1997@gmail.com>
2022-07-05 03:07:59 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Class1
+    {
+        static void Main(string[]args)
+        {
+            int A = 10;
+            int B = 20;
+
+            Console.WriteLine(A + B);
+            Console.WriteLine(A - B);
+            Console.WriteLine(A * B);
+            Console.WriteLine(A / B);
+        }
+
+
+    }
+}
